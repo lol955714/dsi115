04f4a503167f0060dbfb2fcb1bcdd5b61c08e160
lol9519 <32627694+lol9519@users.noreply.github.com>
2020-07-17 12:12:04 -0600

    arreglos mínimos

--- apps/compras/forms.py
+++ apps/compras/forms.py
@@ -23,6 +23,56 @@ class formulario(forms.Form):
 	cantidad=forms.IntegerField(required=False)
 	comentario=forms.CharField(label="Nota especial",max_length=50,required=False)
 
+class ProveedorForm(forms.ModelForm):
+	class Meta:
+		model = Proveedor
+		fields = ('nombre', 'telefono', 'direccion')
+		labels={
+			'nombre':'Nombre del Proveedor',
+			'telefono':'Telefono',
+			'direccion':'Dirección',
+		}
+		widgets={
+			'nombre' : forms.TextInput(attrs={'class':'form-control'}),
+			'telefono' : forms.TextInput(attrs={'class':'form-control'}),
+			'direccion' : forms.TextInput(attrs={'class':'form-control'}),
+		}
+
+class ArticuloForm(forms.ModelForm):
+	class Meta:
+		model = Producto
+		fields = [
+			'nombre', 
+			'descripcion', 
+			'precioventa', 
+			'preciocompra',
+			'existencia',
+			'promocion',
+			'fkproveedor',
+			'fkcategoria',
+		]
+		labels={
+			'nombre':'Nombre Articulo',
+			'descripcion':'Descripción',
+			'precioventa':'Precio de Venta',
+			'preciocompra':'Precio de Compra',
+			'existencia':'Existencia',
+			'promocion':'Promocion',
+			'fkproveedor':'Proveedor',
+			'fkcategoria':'Categoria',
+		}
+		widgets={
+			'nombre' : forms.TextInput(attrs={'class':'form-control'}),
+			'descripcion' : forms.TextInput(attrs={'class':'form-control'}),
+			'precioventa' : forms.NumberInput(attrs={'class':'form-control'}),
+			'preciocompra' : forms.NumberInput(attrs={'class':'form-control'}),
+			'existencia' : forms.NumberInput(attrs={'class':'form-control'}),
+			#'promocion' : forms.ChoiceField(attrs={'class':'form-control'}),
+			'fkproveedor' : forms.Select(attrs={'class':'form-control'}),
+			'fkcategoria' : forms.Select(attrs={'class':'form-control'}),
+		}
+
+
 '''
 class pedidoForm(forms.ModelForm):
 
