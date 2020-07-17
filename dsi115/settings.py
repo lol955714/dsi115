2afb5adec781721b17e488ada5031eba742db273
Edwin <edwin.herrera78@gmail.com>
2020-07-17 15:50:43 -0600

    alerta de inventario
    se volvio  agregar el codigo a la parte del template de existencias

--- dsi115/settings.py
+++ dsi115/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'dsi115.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dsi2',
-        'USER': 'lol',
-        'PASSWORD': 'noquiero',
+        'NAME': 'dsi',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
