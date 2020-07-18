8dbfe96297f98968ab981b165c8abe84383293ed
lol9519 <32627694+lol9519@users.noreply.github.com>
2020-07-17 21:36:48 -0600

    Update admin.py

--- apps/inventario/admin.py
+++ apps/inventario/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-
+'''
 admin.site.register(Categoria)
 
 admin.site.register(Proveedor)
@@ -12,7 +12,7 @@ admin.site.register(Producto)
 admin.site.register(CategoriaIncidencia)
 
 admin.site.register(Incidencia)
-
+'''
 
 admin.sites.AdminSite.site_header = 'Administración del sistema SIAVAC'
 admin.sites.AdminSite.site_title = 'SIAVAC Administracion'
