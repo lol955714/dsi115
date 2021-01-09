bb1a078ee600d3ab7d37a51a0456ef3d95ca6dc6
lol9519 <32627694+lol9519@users.noreply.github.com>
2021-01-08 18:55:18 -0600

    update

--- apps/compras/admin.py
+++ apps/compras/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from apps.compras.models import *
-'''
+
 admin.site.register(detalle_Pedido)
 admin.site.register(Pedido)
 admin.site.register(Tipo_Pago)
-'''
--- apps/ventas/admin.py
+++ apps/ventas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.ventas.models import Empleado, Metas
+from apps.ventas.models import *
 # Register your models here.
 admin.site.register(Empleado)
 admin.site.register(Metas)
